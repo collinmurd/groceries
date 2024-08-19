d08d5ad02a54c4bd2f3fa0715639ffade4c572e1
collinmurd <collinmurdock22@gmail.com>
2024-08-19 18:53:04 -0400

    css modules and centered content

--- app/src/components/App/App.tsx
+++ app/src/components/App/App.tsx
@@ -1,19 +1,21 @@
 import React, { useState } from 'react';
 import { List } from '../List/List';
 
+import classes from './App.module.css';
+
 export function App() {
   const [error, setError] = useState<string>("");
 
   if (error) {
     return (
-      <div className="App">
+      <div className={classes.app}>
         <ErrorPage message={error} />
       </div>
     )
   }
 
   return (
-    <div className="App">
+    <div className={classes.app}>
       <List setError={(error: string) => setError(error)}/>
     </div>
   );
