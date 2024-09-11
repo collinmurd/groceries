53f081b0e72251dd40490c5ed85a1761ec3c1bf9
collinmurd <collinmurdock22@gmail.com>
2024-09-10 22:01:23 -0400

    start on feature meny ui

--- app/src/components/App/App.tsx
+++ app/src/components/App/App.tsx
@@ -2,9 +2,14 @@ import React, { useState } from 'react';
 import { List } from '../List/List';
 
 import classes from './App.module.css';
+import { ActionIcon, Button, Menu } from '@mantine/core';
+import { IconAdjustmentsHorizontal } from '@tabler/icons-react';
+
+type FeatureSet = {[feature: string]: boolean};
 
 export function App() {
   const [error, setError] = useState<string>("");
+  const [features, setFeatures] = useState<FeatureSet>({});
 
   if (error) {
     return (
@@ -21,13 +26,36 @@ export function App() {
   );
 }
 
-
-function ErrorPage(props: {
-  message: string,
-}) {
+function ErrorPage(props: {message: string}) {
   return (
     <div>
       <h2>{props.message}</h2>
     </div>
   )
+}
+
+function FeaturesMenu(props: {features: FeatureSet, setFeatures: (features: FeatureSet) => void}) {
+
+  const featureOptions = Object.entries(props.features).map(f => {
+    const feature = f[0]
+    const enabled = f[1]
+
+    return (
+      <Menu.Item>
+      </Menu.Item>
+    )
+  });
+
+  return (
+    <Menu shadow='md' width={200}>
+      <Menu.Target>
+        <ActionIcon variant="subtle" aria-label="features menu">
+          <IconAdjustmentsHorizontal />
+        </ActionIcon>
+      </Menu.Target>
+      <Menu.Dropdown>
+        {featureOptions}
+      </Menu.Dropdown>
+    </Menu>
+  );
 }
