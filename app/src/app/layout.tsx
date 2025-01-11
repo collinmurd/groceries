7b41fa42ca7b858a979ba11bad8070155f5ed6f5
collinmurd <collinmurdock22@gmail.com>
2025-01-11 14:23:35 -0500

    add max scale to fix zoom on mobile

--- app/src/app/layout.tsx
+++ app/src/app/layout.tsx
@@ -1,10 +1,14 @@
-import type { Metadata } from 'next'
+import type { Metadata, Viewport } from 'next'
 import GlobalProvider from './components/GlobalProvider'
 import classes from './layout.module.css';
  
 export const metadata: Metadata = {
   title: 'Groceries',
   description: 'My grocery list',
+}
+
+export const viewport: Viewport = {
+  maximumScale: 1,
 }
 
 export default function RootLayout({
