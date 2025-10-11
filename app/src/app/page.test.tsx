da5b26d526f792f87e4d7ca3fc95aa97b75e697c
collinmurd <collinmurdock22@gmail.com>
2025-10-11 15:23:04 -0400

    fix tests ;)

--- app/src/app/page.test.tsx
+++ app/src/app/page.test.tsx
@@ -15,25 +15,3 @@ describe('Render', () => {
     expect(screen.findByText('Failed')).rejects.toBeTruthy();
   });
 });
-
-describe('handle errors', () => {
-  it('should display the error page when an error occurs getting features', async () => {
-    (getItems as jest.Mock).mockReturnValue(Promise.resolve([]));
-    (getFeatures as jest.Mock).mockReturnValue(Promise.reject('error'));
-
-    render(<Page />, { wrapper: GlobalProvider });
-
-    expect(await screen.findByText('Failed to get features... try again later')).toBeInTheDocument();
-    expect(screen.findByText('List Content')).rejects.toBeTruthy();
-  });
-
-  it('should display the error page when an error getting list items', async () => {
-    (getFeatures as jest.Mock).mockReturnValue(Promise.resolve([]));
-    (getItems as jest.Mock).mockReturnValue(Promise.reject('error'));
-
-    render(<Page />, { wrapper: GlobalProvider });
-
-    expect(await screen.findByText('Failed to get grocery list... try paper')).toBeInTheDocument();
-    expect(screen.findByText('List Content')).rejects.toBeTruthy();
-  });
-});
