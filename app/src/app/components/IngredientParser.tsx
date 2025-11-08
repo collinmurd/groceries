f78ccbe361ff1b09b1842b5fe72f40cd79841e53
collinmurd <collinmurdock22@gmail.com>
2025-11-08 12:25:17 -0500

    drawer and ingredient parser component

---
+++ app/src/app/components/IngredientParser.tsx
@@ -0,0 +1,10 @@
+
+export function IngredientParser() {
+  // Ingredient parsing logic will go here
+
+  return (
+    <div>
+      <p>Ingredient parser will be implemented here.</p>
+    </div>
+  );
+}
--- app/src/app/page.tsx
+++ app/src/app/page.tsx
@@ -1,17 +1,21 @@
 "use client";
 
 import '@mantine/core/styles.css';
-import { ActionIcon, Group } from '@mantine/core';
+import { ActionIcon, Drawer, Group } from '@mantine/core';
 import { List } from './components/List/List';
 import Link from 'next/link';
-import { IconSettings } from '@tabler/icons-react';
+import { IconAi, IconSettings } from '@tabler/icons-react';
 import { LogoutButton } from './components/LogoutButton';
+import { IngredientParser } from './components/IngredientParser';
+import { useDisclosure } from '@mantine/hooks';
 
 export default function Page() {
   return <App />;
 }
 
 function App() {
+  const [drawerOpened, { open, close }] = useDisclosure(false);
+
   return (
     <div>
       <List />
@@ -23,8 +27,14 @@ function App() {
             </Link>
           </ActionIcon>
           <LogoutButton />
+          <ActionIcon variant="subtle" size="xl" aria-label="ai">
+            <IconAi onClick={open} />
+          </ActionIcon>
         </Group>
       </div>
+      <Drawer opened={drawerOpened} onClose={close} padding="xl" size="xl">
+        <IngredientParser />
+      </Drawer>
     </div>
   );
 }
