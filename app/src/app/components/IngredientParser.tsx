7e2d518569e196f0550c3705d49b51bb12014405
collinmurd <collinmurdock22@gmail.com>
2025-11-09 10:09:56 -0500

    scroll to results

--- app/src/app/components/IngredientParser.tsx
+++ app/src/app/components/IngredientParser.tsx
@@ -1,5 +1,5 @@
 import { Box, Button, Checkbox, LoadingOverlay, SegmentedControl, Textarea, TextInput } from "@mantine/core";
-import { useState } from "react";
+import { forwardRef, useEffect, useRef, useState } from "react";
 import * as api from "../../services/api";
 
 export interface IngredientParserProps {
@@ -13,6 +13,13 @@ export function IngredientParser({ finished }: IngredientParserProps) {
   const [errorMessage, setErrorMessage] = useState<string | null>(null);
   const [parsedIngredients, setParsedIngredients] = useState<string[]>([]);
   const [loading, setLoading] = useState<boolean>(false);
+  const ingredientListRef = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    if (parsedIngredients.length > 0 && ingredientListRef.current) {
+      ingredientListRef.current.scrollIntoView({ behavior: 'smooth', block: 'start' });
+    }
+  }, [parsedIngredients]);
 
   const parseIngredients = () => {
     setErrorMessage(null);
@@ -75,7 +82,7 @@ export function IngredientParser({ finished }: IngredientParserProps) {
       <div>
         <Button style={{ marginTop: 20 }} onClick={parseIngredients}>Parse Ingredients</Button>
       </div>
-      {parsedIngredients.length > 0 && <IngredientListDisplay ingredients={parsedIngredients} setLoading={setLoading} finished={finished} />}
+      {parsedIngredients.length > 0 && <IngredientListDisplay ref={ingredientListRef} ingredients={parsedIngredients} setLoading={setLoading} finished={finished} />}
     </Box >
   );
 }
@@ -86,43 +93,44 @@ interface IngredientListDisplayProps {
   finished: () => void
 }
 
-function IngredientListDisplay({ ingredients, setLoading, finished }: IngredientListDisplayProps) {
-  const [selectedIndicies, setSelectedIndices] = useState<Set<number>>(new Set());
+const IngredientListDisplay = forwardRef<HTMLDivElement, IngredientListDisplayProps>(
+  function IngredientListDisplay({ ingredients, setLoading, finished }, ref) {
+    const [selectedIndicies, setSelectedIndices] = useState<Set<number>>(new Set());
 
-  const handleCheckboxChange = (index: number) => {
-    const newSet = new Set(selectedIndicies);
-    if (newSet.has(index)) {
-      newSet.delete(index);
-    } else {
-      newSet.add(index);
+    const handleCheckboxChange = (index: number) => {
+      const newSet = new Set(selectedIndicies);
+      if (newSet.has(index)) {
+        newSet.delete(index);
+      } else {
+        newSet.add(index);
+      }
+      setSelectedIndices(newSet);
     }
-    setSelectedIndices(newSet);
-  }
 
-  const handleSubmit = async () => {
-    setLoading(true);
-    for (const index of selectedIndicies) {
-      await api.createItem({
-        id: null,
-        description: ingredients[index],
-        section: 'Other',
-        checked: false
-      })
+    const handleSubmit = async () => {
+      setLoading(true);
+      for (const index of selectedIndicies) {
+        await api.createItem({
+          id: null,
+          description: ingredients[index],
+          section: 'Other',
+          checked: false
+        })
+      }
+      setLoading(false);
+      finished();
     }
-    setLoading(false);
-    finished();
-  }
 
-  return (
-    <div>
-      <h3>Parsed Ingredients</h3>
-      <p>Check all ingredients you want to add to the list</p>
-      {ingredients.map((ingredient, index) => (
-        <div key={index} style={{ marginBottom: 5 }}>
-          <Checkbox type="checkbox" checked={selectedIndicies.has(index)} value={ingredient} label={ingredient} onChange={() => handleCheckboxChange(index)} />
-        </div>
-      ))}
-      <Button style={{ marginTop: 20 }} onClick={handleSubmit}>Add Selected Ingredients to List</Button>
-    </div>
-  );
-}
+    return (
+      <div ref={ref} style={{ marginTop: 50 }}>
+        <h3>Parsed Ingredients</h3>
+        <p>Check all ingredients you want to add to the list</p>
+        {ingredients.map((ingredient, index) => (
+          <div key={index} style={{ marginBottom: 5 }}>
+            <Checkbox type="checkbox" checked={selectedIndicies.has(index)} value={ingredient} label={ingredient} onChange={() => handleCheckboxChange(index)} />
+          </div>
+        ))}
+        <Button style={{ marginTop: 20 }} onClick={handleSubmit}>Add Selected Ingredients to List</Button>
+      </div>
+    );
+  });
